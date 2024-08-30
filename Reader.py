d3e1122cb9c2475e1194aa5c4d7f6765b14c206d
Maybe? <154563439+neverfriendme@users.noreply.github.com>
2024-08-30 16:35:48 -0400

    Safety 1

---
+++ Reader.py
@@ -0,0 +1,34 @@
+# by neverfriendme
+def func():
+    Choices = ["read", "quit"]
+    choice = input("read to read the contents of a file or quit:\n").strip().lower()
+    if choice == Choices[0]:
+        directory = input("What is the file you want to open (Please input the directory correctly):\n")
+        if not directory.endswith("/.cpp/.hpp/.h/cxx/.hxx/.py/.pyc/.pyd/.pyo/.pyw/.pyx/.pxd/.pxi/pyzw/.js"):
+            warning = input("Are you sure? The file you are entering could contain malware.Y/N:").strip().lower()
+        if warning == "y":
+            pass
+        elif warning == "n":
+            print("Quiting for your safety")
+            quit()
+        try:
+            with open(directory, "r") as file:
+                text = file.read()
+                print(text)
+        except FileNotFoundError:
+            print("File was not found")
+        except PermissionError:
+            print("You don't have permission to read this file")
+        except IOError:
+            print("An error occurred while trying to read the file")
+        except Exception as e:
+            print("An unexpected error occurred:", str(e))
+    elif choice == Choices[1]:
+        print("Quiting")
+        return
+    else:
+        print("Invalid")
+        return
+
+func()
+
